f25fd6658e3b015cc6c15815fe3db6f259b8caa5
Matthieu Vachon <mvachon@samsaodev.com>
2016-07-12 13:25:27 -0400

    Push specific tag instead of everything (branches included)

--- step.sh
+++ step.sh
@@ -4,7 +4,7 @@ if [ -n "${push}" -a "${push}" == "true" -o "${push}" == "false" ]
 then
     git tag -a ${tag} -m '${tag_message}'
     if [ "${push}" == "true" ]; then
-        git push --follow-tags
+        git push origin ${tag}
     fi
     if (( $? )); then
         echo "Failure" >&2
